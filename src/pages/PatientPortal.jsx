ee30eb6e94f0974d072eeb661f25e5354dd78962
AbdulBariRahmani2 <gbari901817@gmail.com>
2026-09-11 18:29:13 -0800

    logout btn edit

--- src/pages/PatientPortal.jsx
+++ src/pages/PatientPortal.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
 
 export default function PatientPortal() {
+  
   const { user, logout } = useAuth();
   const navigate = useNavigate();
 
