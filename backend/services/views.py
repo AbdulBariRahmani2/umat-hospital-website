b44926904e368442b657432899e2c0d74f78704f
AbdulBariRahmani2 <gbari901817@gmail.com>
2026-09-23 18:36:55 -0800

    pages commit 3

--- backend/services/views.py
+++ backend/services/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
-
 from .models import Service
 from .serializers import ServiceSerializer
-
 
 class ServiceListView(generics.ListAPIView):
     queryset = Service.objects.filter(is_active=True)
