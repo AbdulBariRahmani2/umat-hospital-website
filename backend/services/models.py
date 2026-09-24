66817719c8d2f4f104a22c6f0a399c6874bd254a
AbdulBariRahmani2 <gbari901817@gmail.com>
2026-09-23 19:51:48 -0800

    pages commit 4

--- backend/services/models.py
+++ backend/services/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Service(models.Model):
     slug = models.SlugField(unique=True)
     title = models.CharField(max_length=200)
--- backend/services/serializers.py
+++ backend/services/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Service
 
-
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
